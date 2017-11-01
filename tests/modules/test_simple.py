c889f826622322e85dc8616ab866748ee9e88664
Craig Sebenik <craig@crunchbase.com>
2017-10-31 22:11:29 -0700

    Simplifying the simple test

--- tests/modules/test_simple.py
+++ tests/modules/test_simple.py
@@ -1,23 +1,16 @@
-"""
-Test the simple module.
-"""
-# core python packages
+""" Test the simple module. """
 import imp
 import os
 import unittest
-# third party packages
-# custom packages
 
 
 class TestSimpleModule(unittest.TestCase):
 
     def setUp(self):
-        this_dir = os.path.dirname(os.path.abspath(__file__))
-        tests_dir = os.path.dirname(this_dir)
-        base_dir = os.path.dirname(tests_dir)
-        modules_dir = os.path.join(
-            base_dir, 'salt-files', 'base', '_modules')
-        simple_file = os.path.join(modules_dir, 'simple.py')
+        base_dir = os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        simple_file = os.path.join(
+            base_dir, 'salt-files', 'base', '_modules', 'simple.py')
         self.simple_py = imp.load_source('simple', simple_file)
 
     def test_foo(self):
